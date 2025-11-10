97cdd27f7ebcab413d227ae7edb8008ea511995b
marcelnoehre <marcelnoehre@web.de>
2025-11-10 18:01:43 +0100

    outsource dashboard apps

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Dashboard } from './components/dashboard/dashboard';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
     { path: '', component: Dashboard },
--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import { DashboardItem } from '../interfaces/dashboard-item';
 
+import * as dashboardData from '../../assets/data/dashboard.json';
+
 @Injectable({
   providedIn: 'root'
 })
 export class Data {
+  
   public getDashboardItems(): DashboardItem[] {
-    return [
-      { title: 'ANSI Colors', icon: 'palette', url: '/ansi-colors' },
-      { title: 'Compare Text', icon: 'compare', url: '/compare-text' },
-      { title: 'Text Formatter', icon: 'text_fields', url: '/text-formatter' }
-    ];
+    return dashboardData.apps;
   }
   
 }
