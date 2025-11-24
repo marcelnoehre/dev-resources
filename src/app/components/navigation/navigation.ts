b4e1d64e0b80c1862c9edc895f7415ddee124136
marcelnoehre <marcelnoehre@web.de>
2025-11-24 16:46:14 +0100

    integrate updates from portfolio

--- src/app/components/navigation/navigation.ts
+++ src/app/components/navigation/navigation.ts
@@ -1,9 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Screen } from '../../services/screen';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { NavigationItem } from '../../interfaces/navigation-item';
+import { Data } from '../../services/data';
+import { Theme } from '../../services/theme';
 
 @Component({
   selector: 'app-navigation',
@@ -15,25 +18,41 @@ export class Navigation implements OnInit {
   @Input() home: boolean = false;
   protected width: number = 0;
   protected name: Array<string> = ['M', 'a', 'r', 'c', 'e', 'l', '&nbsp;', 'N', 'ö', 'h', 'r', 'e'];
-  protected items: Array<{ route: string, title: string }> = [
-    { route: '/publications', title: 'Publications' },
-    { route: '/projects', title: 'Projects' },
-    { route: '/cv', title: 'CV' },
-    { route: '/contact', title: 'Contact' },
-    { route: '/impressum', title: 'Impressum' }
-  ];
+  protected navigation: NavigationItem[] = [];
+  protected path: string = '';
+  protected themeIcon: string = '';
 
   constructor(
     private _router: Router,
-    private _screen: Screen
+    private _route: ActivatedRoute,
+    private _screen: Screen,
+    private _theme: Theme,
+    private _data: Data
   ) { }
 
   ngOnInit() {
     this._screen.width$.subscribe(w => this.width = w);
+    this.navigation = this._data.navigation;
+    this.path = '/' + this._route.snapshot.routeConfig?.path;
+    this.themeIcon = this._theme.isDarkMode() ? 'light_mode' : 'dark_mode';
   }
 
-  protected redirect(route: string): void {
-    this._router.navigate([route]);
+  protected redirect(route: string | undefined): void {
+    if (route) {
+      this._router.navigate([route]).then(() => {
+        window.scrollTo(0, 0)
+      });
+    } else {
+      window.scrollTo({
+        top: window.document.body.scrollHeight,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  toggleTheme() {
+    this._theme.toggleTheme();
+    this.themeIcon = this._theme.isDarkMode() ? 'light_mode' : 'dark_mode';
   }
 
 }
--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { DashboardItem } from '../interfaces/dashboard-item';
+import { NavigationItem } from '../interfaces/navigation-item';
 import { ContactItem } from '../interfaces/contact-item';
 
 import * as dashboardData from '../../assets/data/dashboard.json';
+import * as navData from '../../assets/data/navigation.json';
 import * as contactData from '../../assets/data/contact.json';
 
 @Injectable({
@@ -12,6 +14,10 @@ export class Data {
   
   public getDashboardItems(): DashboardItem[] {
     return dashboardData.apps;
+  }
+
+  public get navigation(): NavigationItem[] {
+    return navData.navigation;
   }
 
   public get contact(): ContactItem[] {
